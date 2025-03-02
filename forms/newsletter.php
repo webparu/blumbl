b8e4d1eae5b1d0de404b473def8527e57bcf6d3b
webparu <web.irakli@gmail.com>
2025-03-02 20:12:19 +0400

    Add files via upload

--- forms/newsletter.php
+++ forms/newsletter.php
@@ -1,5 +1,6 @@
 <?php
-  $receiving_email_address = 'contact@blumbl.ge';
+
+  $receiving_email_address = 'info@blumbl.ge';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -11,9 +12,9 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
+  $contact->from_name = $_POST['email'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->subject ="New Subscription: " . $_POST['email'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -25,9 +26,7 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
